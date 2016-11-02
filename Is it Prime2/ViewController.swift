7946bf05b26b84243c8c921a891308b14a0859be
jim leach <yogajim@gmail.com>
2016-11-02 15:25:12 -0400

    Initial commit

---
+++ Is it Prime2/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Is it Prime2
+//
+//  Created by James Leach on 11/2/16.
+//  Copyright © 2016 Dadio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBAction func isItPrime(_ sender: AnyObject) {
+        
+        if let userEnteredString = numberTextField.text {
+            
+            let userEnteredInteger = Int(userEnteredString)
+            
+            if let number = userEnteredInteger {
+                var isPrime = true
+                
+                if number == 1 {
+                    
+                    isPrime = false
+                }
+                
+                var i = 2
+                
+                while i < number {
+                    
+                    if number % i == 0 {
+                        
+                        isPrime = false
+                    }
+                    
+                    i += 1
+                    
+                    
+                }
+                if isPrime {
+                    resultLabel.text = "\(number) is prime"
+                } else {
+                    resultLabel.text = "\(number) is not prime"
+                }
+                
+            } else {
+                
+                resultLabel.text = "Please enter a positive whole number"
+            }
+            
+            
+        }
+        
+       
+        
+        
+    }
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
